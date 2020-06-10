2f5f09a400b2a55595e9a14715982b48a35eaae5
karthik <nallaboluk@gmail.com>
2020-06-10 12:19:34 +0530

    adding finding whether bet won or lose

--- gambling.sh
+++ gambling.sh
@@ -1,4 +1,21 @@
 #!/bin/bash -x
 
+#!/bin/bash -x
+
 STAKE=100
 BET_PER_MATCH=1
+
+function findResultOfBet {
+
+	if [ $((RANDOM%2)) -eq 0 ]
+	then
+		echo 1
+	else
+		echo 0
+	fi
+
+}
+
+result=$(findResultOfBet)
+echo $result
+
