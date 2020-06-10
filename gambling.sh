7a731fa20aeb4e8c04bde0b2626bb2ea717abb0c
karthik <nallaboluk@gmail.com>
2020-06-10 12:37:48 +0530

    adding amount won for a day

--- gambling.sh
+++ gambling.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -x
 
-#!/bin/bash -x
-
 STAKE=100
 BET_PER_MATCH=1
+
 
 function findResultOfBet {
 
@@ -16,6 +15,38 @@ function findResultOfBet {
 
 }
 
-result=$(findResultOfBet)
-echo $result
+
+
+function findAmountWonPerDay {
+
+
+	read -p "enter the percentage of won or loss of stake to stop betting for that day" percent
+
+	local totalAmountPerDay=0
+	local variableAmount=$(($percent*$STAKE))
+	variableAmount=$(($variableAmount/100))
+	local maxAmount=$variableAmount
+	local minAmount=$((-1*$variableAmount))
+
+	while [ $totalAmountPerDay -lt $maxAmount -a $totalAmountPerDay -gt $minAmount ]
+	do
+		local result=$(findResultOfBet)
+		if [ $result -eq 1 ]
+		then
+			totalAmountPerDay=$(($totalAmountPerDay+$BET_PER_MATCH))
+		else
+			totalAmountPerDay=$(($totalAmountPerDay-$BET_PER_MATCH))
+		fi
+	done
+
+	echo $totalAmountPerDay
+}
+
+
+totalAmountWonPerDay=$(findAmountWonPerDay)
+
+echo $totalAmountWonPerDay
+
+
+
 
